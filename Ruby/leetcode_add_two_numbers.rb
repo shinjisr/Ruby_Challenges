3e8dc27f0b49c8bceeada8b7e4e940c3bcf1b0ee
Steven C <stevencch99@gmail.com>
2020-05-04 13:38:43 +0800

    Update leetcode_add_two_numbers.rb

--- Ruby/leetcode_add_two_numbers.rb
+++ Ruby/leetcode_add_two_numbers.rb
@@ -16,7 +16,8 @@ l1 = ListNode.new(2, ln4)
 l2 = ListNode.new(5, ln6)
 # Input: [2, 4, 3], [5, 6, 4]
 # Output: [7, 0, 8]
-
+#
+# My solution: Runtime faster than 28.17%, Memory usage less than 85.71%
 def add_two_numbers(l1, l2)
   tmp1 = [l1]
   tmp2 = [l2]
@@ -42,4 +43,19 @@ def add_two_numbers(l1, l2)
   res
 end
 
+# Other way, ref: https://leetcode.com/problems/add-two-numbers/discuss/244039/Elegant-Ruby-solution
+# 12.48, 100%
+def add_two_numbers(l1, l2)
+  modulus, previous = 0, (dummy_head = ListNode.new nil)
+
+  while l1 || l2 || modulus.nonzero?
+    modulus, quotient = [l1&.val, l2&.val, modulus].compact.sum.divmod 10
+    previous, l1, l2 = (previous.next = ListNode.new quotient), l1&.next, l2&.next
+  end
+
+  dummy_head.next
+end
+
+# Input: [2, 4, 3], [5, 6, 4]
+# Output: [7, 0, 8]
 p add_two_numbers(l1, l2)
