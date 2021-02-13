f0ceb2121055f17ffd576bdfd277468e2ed4c16f
Steven C <stevencch99@gmail.com>
2021-02-13 14:26:53 +0800

    Update leetcode_258_add_digits.rb

--- Ruby/leetcode_258_Add_digits.rb
+++ Ruby/leetcode_258_Add_digits.rb
@@ -6,19 +6,57 @@
 # Output: 2
 # Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
 
-def add_digits(num)
-  if num > 9
-    result = num.to_s.chars.map(&:to_i).reduce(:+)
-    add_digits(result)
-  else
-    p num
+def add_digits1(num)
+  while num > 9
+    num = num.to_s.chars.map(&:to_i).reduce(:+)
+  end
+  num
+end
+
+def add_digits2(num)
+  return num if num < 10
+  num % 9
+end
+
+def add_digits3(num)
+  while num > 9
+    next_digit = 0
+
+    while num > 0
+      next_digit += num % 10
+      num /= 10
+    end
+
+    num = next_digit # if num greater than 9, then go to the next loop 
+  end
+
+  num
+end
+
+def test_res(method)
+  test_case = [101, 38, 22]
+
+  test_case.each do |e|
+    puts send(method, e)
   end
 end
 
-def add_digits(num)
-  p num && (num % 9 || 9)
+# test_res(:add_digits1)
+
+# p add_digits3(101) # shoud be 2
+# p add_digits3(38)  # shoud be 2
+# p add_digits3(22)  # shoud be 4
+
+require 'benchmark'
+
+def performance_test(*methods)
+  n = 1_000_000
+  p test_case = rand(0..200)
+  Benchmark.bm do |x|
+    methods.each do |method|
+      x.report(method) { n.times { send(method, test_case) } }
+    end
+  end
 end
 
-add_digits(101) # shoud be 2
-add_digits(38)  # shoud be 2
-add_digits(22)  # shoud be 4
+performance_test(:add_digits1, :add_digits2, :add_digits3)
